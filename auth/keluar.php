d30931f4186eabac29e8bdf95674f765caa4dfbf
Raflyromeo <raflyromeonasution07@gmail.com>
2026-06-12 02:33:31 +0700

    refactor: Memperbarui halaman autentikasi keluar.php

--- auth/keluar.php
+++ auth/keluar.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
-// Hapus semua data sesi
 $_SESSION = [];
 
-// Jika menggunakan cookie sesi, hapus juga
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -12,10 +10,8 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Hancurkan sesi
 session_destroy();
 
-// Redirect ke halaman masuk
 header("Location: masuk.php");
 exit;
 ?>
