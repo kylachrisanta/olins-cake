589d3cc0685a542e4df0f7738ba1bdf7a0e1c70e
Raflyromeo <raflyromeonasution07@gmail.com>
2026-06-12 02:33:29 +0700

    refactor: Membersihkan komentar dan merapikan kode di keluar_admin.php

--- admin/keluar_admin.php
+++ admin/keluar_admin.php
@@ -1,12 +1,10 @@
 <?php
 session_start();
 
-// Hapus variabel session admin
 unset($_SESSION['status_login_admin']);
 unset($_SESSION['id_admin']);
 unset($_SESSION['nama_admin']);
 
-// Redirect ke login admin
 header("Location: masuk_admin.php");
 exit;
 ?>
